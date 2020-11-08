ebb6974c555d415a52435966a377c1b94541eeef
JenniferS <nfjennifer90@gmail.com>
2020-11-07 20:10:45 -0500

    Restful API test

---
+++ src/test/java/com/ramsoft/AutomationTest/BaseTest.java
@@ -0,0 +1,5 @@
+package com.ramsoft.AutomationTest;
+
+public class BaseTest {
+
+}
---
+++ src/test/java/com/ramsoft/AutomationTest/TestInvitation.java
@@ -0,0 +1,13 @@
+package com.ramsoft.AutomationTest;
+
+import org.testng.annotations.Test;
+
+public class TestInvitation extends BaseTest {
+
+	@Test
+	public void test01() {
+
+		System.out.println("hello world!");
+	}
+
+}
